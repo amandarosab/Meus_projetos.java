615e836c7763e966a840672c7df10243162b6987
Amanda Rosa <amandabelo.contato@outlook.com>
2024-10-03 13:45:11 -0300

    Update Calculadora_IMC.java

--- Calculadora_IMC/src/Calculadora_IMC.java
+++ Calculadora_IMC/src/Calculadora_IMC.java
@@ -10,39 +10,39 @@ public class App {
         System.out.println("Agora digite o seu peso: ");
         Scanner peso = new Scanner(System.in);
         double peso1 = peso.nextFloat();
-        double cal_altura = (altura1*altura1);
-        double cal_imc = (peso1/cal_altura);
+        double cal_Altura = (altura1*altura1);
+        double cal_Imc = (peso1/cal_Altura);
 
-        if (cal_imc <= 16) {
-            System.out.printf("Você está com Magreza Grave, o seu IMC é de %.1f", cal_imc);
+        if (cal_Imc <= 16) {
+            System.out.printf("Você está com Magreza Grave, o seu IMC é de %.1f", cal_Imc);
         }
         
-        else if ((cal_imc >= 16) && (cal_imc < 16.9)) {
-            System.out.printf("Você está com Magreza Moderada, o seu IMC é de %.1f !", cal_imc);
+        else if ((cal_Imc >= 16) && (cal_Imc < 16.9)) {
+            System.out.printf("Você está com Magreza Moderada, o seu IMC é de %.1f !", cal_Imc);
         }
 
-        else if ((cal_imc >= 17) && (cal_imc < 18.5)) {
-            System.out.printf("Você está com Magreza Leve, o seu IMC é de %.1f !", cal_imc);
+        else if ((cal_Imc >= 17) && (cal_Imc < 18.5)) {
+            System.out.printf("Você está com Magreza Leve, o seu IMC é de %.1f !", cal_Imc);
         }
 
-        else if ((cal_imc >= 18.6) && (cal_imc < 24.9)) {
-            System.out.printf("Você está no Peso Ideal, o seu IMC é de %.1f !", cal_imc);
+        else if ((cal_Imc >= 18.6) && (cal_Imc < 24.9)) {
+            System.out.printf("Você está no Peso Ideal, o seu IMC é de %.1f !", cal_Imc);
         }
 
-        else if ((cal_imc >= 25) && (cal_imc < 29.9)) {
-            System.out.printf("Você está com Sobrepeso, o seu IMC é de %.1f !", cal_imc);
+        else if ((cal_Imc >= 25) && (cal_Imc < 29.9)) {
+            System.out.printf("Você está com Sobrepeso, o seu IMC é de %.1f !", cal_Imc);
         }
 
-        else if ((cal_imc >= 30) && (cal_imc < 34.9)) {
-            System.out.printf("Você está com Obesidade Grau I, o seu IMC é de %.1f !", cal_imc);
+        else if ((cal_Imc >= 30) && (cal_Imc < 34.9)) {
+            System.out.printf("Você está com Obesidade Grau I, o seu IMC é de %.1f !", cal_Imc);
         }
 
-        else if ((cal_imc >= 35) && (cal_imc < 39.9)) {
-            System.out.printf("Você está com Obesidade Grau II ou Severa, o seu IMC é de %.1f !", cal_imc);
+        else if ((cal_Imc >= 35) && (cal_Imc < 39.9)) {
+            System.out.printf("Você está com Obesidade Grau II ou Severa, o seu IMC é de %.1f !", cal_Imc);
         }
 
-        else if (cal_imc >= 40) {
-            System.out.printf("Você está com Obesidade Grau III ou Mórbida, o seu IMC é de %.1f !", cal_imc);
+        else if (cal_Imc >= 40) {
+            System.out.printf("Você está com Obesidade Grau III ou Mórbida, o seu IMC é de %.1f !", cal_Imc);
         }
 
         altura.close();
